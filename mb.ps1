8c3cc29a5de8dd9a1109ef19f47e6781f250029c
Zauzanov <zauzanov@gmail.com>
2026-04-18 02:51:43 +0300

    Add dest folder

--- mb.ps1
+++ mb.ps1
@@ -3,3 +3,12 @@ $source = "C:\Users\user\Downloads\UT"
 
 # Path to backup destination folder
 $destinationFolder = "E:\Backups\UT"
+
+# Create the destination folder if it does'nt exist
+if (!(Test-Path $destinationFolder)) {
+    New-Item -ItemType Directory -Path $destinationFolder
+}
+
+# Format the current date for the filename
+$date = Get-Date -Format "yyyyMMdd"
+
